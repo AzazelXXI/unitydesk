8d5d7929f369c0f5aeffe3938f950b07c551fe19
Azazel <trunghoang1124@gmail.com>
2025-04-10 20:24:54 +0700

    HTTPS OK

--- src/main.py
+++ src/main.py
@@ -1,20 +1,34 @@
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
-from fastapi import staticfiles
-from manager import MeetingManager
+from starlette.staticfiles import StaticFiles
+from src.manager import MeetingManager
 import ssl
+import logging
+from logging.handlers import RotatingFileHandler
+import asyncio
+
+# Configure logging
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log_file = "app.log"
+log_handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=2)
+log_handler.setFormatter(log_formatter)
+log_handler.setLevel(logging.INFO)
+
+app_logger = logging.getLogger("fastapi")
+app_logger.setLevel(logging.INFO)
+app_logger.addHandler(log_handler)
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain('cert.pem', keyfile='key.pem')
 
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="src/templates")
 
 meeting_manager = MeetingManager()
 
 app = FastAPI()
-app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 
 app.add_middleware(
@@ -24,6 +38,16 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
+# Log uncaught exceptions
+@app.middleware("http")
+async def log_exceptions(request: Request, call_next):
+    try:
+        response = await call_next(request)
+        return response
+    except Exception as e:
+        app_logger.exception("Unhandled exception occurred")
+        raise e
 
 @app.get("/")
 async def home(request: Request):
@@ -53,3 +77,13 @@ async def websocket_endpoint(websocket: WebSocket, room_name: str, client_id: st
 @app.get("/room/{roomName}")
 def get_video(request: Request, roomName:str):
     return templates.TemplateResponse(request=request, name="index.html")
+
+# Gracefully handle asyncio.CancelledError
+@app.on_event("shutdown")
+async def shutdown_event():
+    try:
+        # Perform any cleanup tasks here
+        app_logger.info("Application is shutting down...")
+    except asyncio.CancelledError:
+        app_logger.warning("Shutdown process was interrupted by CancelledError.")
+        raise
