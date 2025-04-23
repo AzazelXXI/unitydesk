098298b176ae2b584c1344ff34cc36ff6ec57135
danthienphuc <danthienphuc@gmail.com>
2025-04-23 13:51:50 +0700

    migrate database

--- src/database.py
+++ src/database.py
@@ -1,20 +1,37 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from asyncio import current_task
+from typing import AsyncGenerator, Any
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_scoped_session,
+)
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Get database URL from environment variable with a fallback for local development
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/csa_hello")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql+asyncpg://postgres:#0Comatkhau@localhost:5432/csa_hello"
+)
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    # pool_size=20, max_overflow=0
+)
 
 Base = declarative_base()
 
-# Dependency to get DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Dependency for getting async DB session
+async def get_db() -> AsyncGenerator[AsyncSession, Any]:
+    # expire_on_commit=False will prevent attributes from being expired
+    # after commit.
+    session_maker = sessionmaker(
+        bind=engine, 
+        expire_on_commit=False, 
+        class_=AsyncSession
+    )
+    Session = async_scoped_session(session_maker, scopefunc=current_task)
+    async with Session() as session:
+        yield session
